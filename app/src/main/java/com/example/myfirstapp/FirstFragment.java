4f1da8a47c21cd52e347124cd2382b68343e4a77
Todd Erickson <twericks@usc.edu>
2020-04-23 14:38:35 -0700

    Through Lesson 8

--- app/src/main/java/com/example/myfirstapp/FirstFragment.java
+++ app/src/main/java/com/example/myfirstapp/FirstFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,13 +13,19 @@ import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment {
 
+    TextView showCountTextView;
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View fragmentFirstLayout = inflater.inflate(R.layout.fragment_first, container, false);
+        // Get the count text view
+        showCountTextView = fragmentFirstLayout.findViewById(R.id.textview_first);
+
+        return fragmentFirstLayout;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -30,5 +38,30 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+
+        view.findViewById(R.id.toast_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast myToast = Toast.makeText(getActivity(), R.string.toast_text, Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+
+        view.findViewById(R.id.count_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countMe(view);
+            }
+        });
+    }
+
+    private void countMe(View view) {
+        // Get the value of the text view
+        String countString = showCountTextView.getText().toString();
+        // Convert value to a number and increment it
+        Integer count = Integer.parseInt(countString);
+        count++;
+        // Display the new value in the text view.
+        showCountTextView.setText(count.toString());
     }
 }
